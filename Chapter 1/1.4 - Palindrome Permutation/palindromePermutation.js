619f12c00a921c7dcef3918c81c0b80510886760
Galen Casstevens <GCasstevens@noregon.com>
2022-01-11 17:12:41 -0500

    completed 1.4 solution

--- Chapter 1/1.4 - Palindrome Permutation/palindromePermutation.js
+++ Chapter 1/1.4 - Palindrome Permutation/palindromePermutation.js
@@ -1,18 +1,21 @@
 // Given a string, write a function to determine if it is a permutation of a palindrome
 // Tact Coa -> taco cat
-// 0 1 2 3 4
-let isPermutationOfPalindrome = (string) => {
-    for (let i = 0; i < string.length; i++) {
-        var regex = new RegExp(string[i], 'g');
-        var count = (string.toLowerCase().match(regex)||[]).length;
-        //
 
+let isPermutationOfPalindrome = (string) => {
+    var middleChar = false;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== ' ') {
+            var regex = new RegExp(string[i].toLowerCase(), 'g');
+            var count = (string.toLowerCase().match(regex) || []).length;
+            if (count % 2 !== 0 && middleChar === false) middleChar = true;
+            else if (count % 2 !== 0 && middleChar) return false;
+        }
     }
+    return true;
 };
 
-var text = 'Taco Cat';
-text = text.toLowerCase();
+var text = 'Ta ct Coa ';
+var text2 = 'redivider';
+var text3 = 'deifiedf';
 
-console.log(text[1]);
-var regexA = new RegExp(text[1], 'g');
-console.log((text.match(regexA).length));
+console.log(isPermutationOfPalindrome(text3));
