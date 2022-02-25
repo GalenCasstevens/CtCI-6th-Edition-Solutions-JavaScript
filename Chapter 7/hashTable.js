63c1a645cdc3734c32b0be4fdc187a4a77b971f3
Galen Casstevens <galencasstevens@gmail.com>
2022-02-25 15:25:23 -0500

    changed tests for hash table slightly

--- Chapter 7/hashTable.js
+++ Chapter 7/hashTable.js
@@ -1,13 +1,8 @@
 // Design and implement a hash table which uses chaining (linked lists) to handle collisions.
+
 var LinkedList = function (key, value) {
 	this.keyValuePair = [key, value];
 	this.next = null;
-};
-
-LinkedList.prototype.find = function (key) {
-	if (this.keyValuePair[0] === key) return this.keyValuePair[1];
-	else if (this.next !== null) return this.next.find(key);
-	else return null;
 };
 
 LinkedList.prototype.insert = function (key, value) {
@@ -15,19 +10,25 @@ LinkedList.prototype.insert = function (key, value) {
 	else this.next.insert(key, value);
 };
 
-LinkedList.prototype.replace = function (key, value) {
-	if (this.keyValuePair[0] === key) this.keyValuePair = [key, value];
-	else this.next.replace(key, value);
+LinkedList.prototype.find = function (key) {
+	if (this.keyValuePair[0] === key) return this.keyValuePair[1];
+	else if (this.next !== null) this.next.find(key, value);
+	else return null;
 };
 
-LinkedList.prototype.delete = function (key) {
+LinkedList.prototype.replace = function (key, value) {
+	if (this.keyValuePair[0] === key) this.keyValuePair = [key, value];
+	else return this.next.replace(key, value);
+};
+
+LinkedList.prototype.devare = function (key) {
 	if (this.keyValuePair[0] === key) {
-		// delete node
-	} else this.next.delete(key);
+		// devare
+	} else this.next.devare(key);
 };
 
 var getHash = function (key, limit) {
-	if (typeof key !== 'string') throw 'error, key is not a string';
+	if (typeof key !== 'string') throw 'error, key not a string';
 	else {
 		var result = 0;
 		for (var i = 0; i < key.length; i++) {
@@ -51,24 +52,19 @@ HashTable.prototype.insert = function (key, value) {
 
 HashTable.prototype.retrieve = function (key) {
 	var hash = getHash(key, this.limit);
-	if (this.array[hash] === undefined) throw 'key does not exist';
+	if (this.array[hash] === undefined) throw 'key not found';
 	else return this.array[hash].find(key);
 };
 
 HashTable.prototype.replace = function (key, value) {
 	var hash = getHash(key, this.limit);
-	if (this.array[hash] === undefined) throw 'key does not exist';
+	if (this.array[hash] === undefined) throw 'key not found';
 	else this.array[hash].replace(key, value);
 };
 
-HashTable.prototype.delete = function (key) {
-	var hash = getHash(key, this.limit);
-	if (this.array[hash] === undefined) throw 'key does not exist';
-	else this.array[hash].delete(key);
-};
-
 var ht = new HashTable();
-ht.insert('12319', 'secret doc 1');
-ht.insert('23435', 'top secret doc 2');
-console.log(ht);
-console.log(ht.retrieve('23435'));
+ht.insert('242341', 'Cracking the Coding Interview');
+ht.insert('544214', 'JavaScript: The Good Parts');
+console.log(ht.retrieve('544214'));
+ht.replace('544214', "The Complete Software Developer's Career Guide");
+console.log(ht.retrieve('544214'));
