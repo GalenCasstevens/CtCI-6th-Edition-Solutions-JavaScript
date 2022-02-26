9c1f3707fde9fd43e6495d1d7c705fcf16ff52a6
Galen Casstevens <galencasstevens@gmail.com>
2022-02-26 06:12:16 -0500

    finished sorted matrix search with some bugs

--- Chapter 10/sortedMatrixSearch.js
+++ Chapter 10/sortedMatrixSearch.js
@@ -11,26 +11,40 @@ class Range {
 const matrix = [
 	[0, 20, 25, 40],
 	[10, 30, 35, 45],
-	[15, 40, 51, 55],
+	[15, 44, 51, 55],
 	[20, 50, 55, 60],
 	[21, 53, 60, 65],
 ];
 
 let findElement = (matrix, element, rowRange, colRange) => {
-	var m = matrix.length; // number of rows
-	var n = matrix[0].length; // number of columns
+	var m = matrix.length - 1; // max row index
+	var n = matrix[0].length - 1; // max col index
 	if (rowRange === undefined) rowRange = new Range(0, m);
 	if (colRange === undefined) colRange = new Range(0, n);
 
 	var midRow = Math.floor(rowRange.start + (rowRange.end - rowRange.start) / 2);
 	var midCol = Math.floor(colRange.start + (colRange.end - colRange.start) / 2);
 
-	if (element < matrix[midRow][midCol])
-		findElement(matrix, element, new Range(0, midRow), new Range(0, midCol));
-	else findElement(matrix, element, new Range(midRow, m), new Range(midCol, n));
+	if (
+		(midRow === rowRange.start || midCol === colRange.start) &&
+		element !== matrix[midRow][midCol]
+	)
+		return -1;
+	else if (element === matrix[midRow][midCol]) return 1;
+	else if (element < matrix[midRow][midCol])
+		return findElement(
+			matrix,
+			element,
+			new Range(0, midRow),
+			new Range(0, midCol)
+		);
+	else
+		return findElement(
+			matrix,
+			element,
+			new Range(midRow, m),
+			new Range(midCol, n)
+		);
 };
 
-var m = matrix.length; // number of rows
-var n = matrix[0].length; // number of columns
-
-console.log(matrix[m][n]);
+console.log(findElement(matrix, 60));
