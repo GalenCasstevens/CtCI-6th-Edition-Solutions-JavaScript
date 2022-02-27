a109022518cbea0ec2a26874628f4caf5d6bea96
Galen Casstevens <galencasstevens@gmail.com>
2022-02-27 08:09:17 -0500

    updated sorted matrix search alg

--- Chapter 10/sortedMatrixSearch.js
+++ Chapter 10/sortedMatrixSearch.js
@@ -1,50 +1,28 @@
 // Given an M x N matrix in which each row and each column is sorted in ascending order,
 // write a method to find an element.
 
-class Range {
-	constructor(start, end) {
-		this.start = start;
-		this.end = end;
-	}
-}
-
 const matrix = [
-	[0, 20, 25, 40],
-	[10, 30, 35, 45],
-	[15, 44, 51, 55],
-	[20, 50, 55, 60],
-	[21, 53, 60, 65],
+	[10, 15, 25, 44],
+	[11, 16, 30, 50],
+	[15, 20, 33, 51],
+	[20, 23, 44, 555],
+	[34, 30, 444, 777],
 ];
 
-let findElement = (matrix, element, rowRange, colRange) => {
-	var m = matrix.length - 1; // max row index
-	var n = matrix[0].length - 1; // max col index
-	if (rowRange === undefined) rowRange = new Range(0, m);
-	if (colRange === undefined) colRange = new Range(0, n);
+let findElement = (matrix, element, minRowInd, maxColInd) => {
+	if (minRowInd === undefined) minRowInd = 0;
+	if (maxColInd === undefined) maxColInd = matrix[0].length - 1;
+	if (maxColInd < 0 || minRowInd > matrix.length - 1) return -1;
+	var topRightVal = matrix[minRowInd][maxColInd];
 
-	var midRow = Math.floor(rowRange.start + (rowRange.end - rowRange.start) / 2);
-	var midCol = Math.floor(colRange.start + (colRange.end - colRange.start) / 2);
-
-	if (
-		(midRow === rowRange.start || midCol === colRange.start) &&
-		element !== matrix[midRow][midCol]
-	)
-		return -1;
-	else if (element === matrix[midRow][midCol]) return 1;
-	else if (element < matrix[midRow][midCol])
-		return findElement(
-			matrix,
-			element,
-			new Range(0, midRow),
-			new Range(0, midCol)
-		);
-	else
-		return findElement(
-			matrix,
-			element,
-			new Range(midRow, m),
-			new Range(midCol, n)
-		);
+	if (topRightVal === element) return 1;
+	else if (element < topRightVal) {
+		maxColInd--;
+		return findElement(matrix, element, minRowInd, maxColInd);
+	} else if (element > topRightVal) {
+		minRowInd++;
+		return findElement(matrix, element, minRowInd, maxColInd);
+	}
 };
 
-console.log(findElement(matrix, 60));
+console.log(findElement(matrix, 51));
