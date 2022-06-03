a6332f7bcc355ec69de8611c5e948357a74e628b
Galen Casstevens <galencasstevens@gmail.com>
2022-06-02 22:29:49 -0400

    added solution for 2.6 - Determine if Linked List is Palindrome

---
+++ Chapter 2/2.6 - Palindrome/palindrome.js
@@ -0,0 +1,41 @@
+// Implement a function to determine if a linked list is a palindrome.
+
+let LinkedList = require('../util/linkedList');
+
+let isStrEven = (str) => str.length % 2 == 0;
+
+let isPalindrome = (list) => {
+	var ptr = list.head,
+		text = '';
+
+	while (ptr) {
+		text += ptr.value;
+		ptr = ptr.next;
+	}
+
+	for (let i = 0; i < text.length; i++) {
+		var endInd = text.length - (i + 1);
+		if (!isStrEven(text) && i !== text.length / 2 - 1) {
+			if (text.charAt(i) !== text.charAt(endInd)) return false;
+		} else if (isStrEven(text) && text.charAt(i) !== text.charAt(endInd))
+			return false;
+	}
+
+	return true;
+};
+
+let list = new LinkedList();
+for (let item of ['r', 'a', 'c', 'e', 'c', 'a', 'r']) {
+	list.append(item);
+}
+let list2 = new LinkedList();
+for (let item of ['h', 'a', 'n', 'n', 'a', 'h']) {
+	list2.append(item);
+}
+let list3 = new LinkedList();
+for (let item of ['r', 'a', 'i', 'n']) {
+	list3.append(item);
+}
+console.log(isPalindrome(list)); // true
+console.log(isPalindrome(list2)); // true
+console.log(isPalindrome(list3)); // false
